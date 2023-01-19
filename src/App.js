3195bb9b49eab7fc79655d207b2b7d7b35ef908e
bendai94 <bendai94@gmail.com>
2023-01-19 17:57:15 -0500

    changed message

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          I was changed
+          Deploy attempt.
         </p>
         <a
           className="App-link"
